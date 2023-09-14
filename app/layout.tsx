f8f77da8928f2e8ee8f58f214bcf9ce69425f371
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-14 13:19:54 +0400

    Update theme usage and meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@mantine/core/styles.css';
 import React from 'react';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
+import { theme } from '../theme';
 
 export const metadata = {
   title: 'Mantine Next.js template',
@@ -13,9 +14,13 @@ export default function RootLayout({ children }: { children: any }) {
       <head>
         <ColorSchemeScript />
         <link rel="shortcut icon" href="/favicon.svg" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
       </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
       </body>
     </html>
   );
--- theme.ts
+++ theme.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
