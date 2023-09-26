9a4bbde42b38ee9a7a0e2f875bbd9c11f7c88623
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-26 12:34:09 +0400

    Remove deprecated color prop from text

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -10,7 +10,7 @@ export function Welcome() {
           Mantine
         </Text>
       </Title>
-      <Text color="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
         This starter Next.js project includes a minimal setup for server side rendering, if you want
         to learn more on Mantine + Next.js integration follow{' '}
         <Anchor href="https://mantine.dev/guides/next/" size="lg">
