b63d556e6aa85e88ab0ec138cf148e13675eb7c1
Vitaly Rtishchev <rtivital@gmail.com>
2023-10-02 16:52:00 +0400

    Add optimize package imports

--- next.config.js
+++ next.config.js
@@ -7,4 +7,7 @@ module.exports = withBundleAnalyzer({
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
 });
