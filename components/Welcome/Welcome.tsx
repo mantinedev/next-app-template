9f77f6e45f9b67f81fc4df3b2c639334e32f63d5
metaproph3t <metaproph3t@protonmail.com>
2023-11-19 00:00:00 +0000

    Clean up welcome page

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -5,14 +5,13 @@ export function Welcome() {
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
+        Welcome to the{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
           Meta-DAO
         </Text>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        The Meta-DAO is a new cybernetic institution managed by programs stored on the Solana
-        blockchain. Instead of token-voting or liquid democracy, the Meta-DAO uses futarchy.
+        The Meta-DAO is the first organization in human history that's governed by markets, not politics.
       </Text>
     </>
   );
