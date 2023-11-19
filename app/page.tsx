662792846f00a420272e367779e63f2ec5c7ca52
metaproph3t <metaproph3t@protonmail.com>
2023-11-19 00:00:00 +0000

    Remove color scheme toggle
    Idk, dark mode doesn't look right imo :shrug:

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Layout } from '../components/Layout/Layout';
 
 export default function HomePage() {
   return (
     <Layout>
       <Welcome />
-      <ColorSchemeToggle />
     </Layout>
   );
 }
