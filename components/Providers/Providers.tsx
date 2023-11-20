90f1b4ba0b512f65d0a5a9275585cd24085eb504
metaproph3t <metaproph3t@protonmail.com>
2023-11-20 00:00:00 +0000

    Add `LedgerWalletAdapter`

--- components/Providers/Providers.tsx
+++ components/Providers/Providers.tsx
@@ -4,7 +4,11 @@ import React, { useCallback, useMemo } from 'react';
 import { MantineProvider } from '@mantine/core';
 import { WalletError } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  LedgerWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { Notifications } from '@mantine/notifications';
 import { theme } from '../../theme';
@@ -14,7 +18,10 @@ import { AutocratProvider } from '../../contexts/AutocratContext';
 export function Providers({ children }: { children: React.ReactNode }) {
   const { endpoint } = useNetworkConfiguration();
 
-  const wallets = useMemo(() => [new SolflareWalletAdapter(), new PhantomWalletAdapter()], []);
+  const wallets = useMemo(
+    () => [new SolflareWalletAdapter(), new PhantomWalletAdapter(), new LedgerWalletAdapter()],
+    [],
+  );
 
   const onError = useCallback((error: WalletError) => {
     console.error(error);
--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -11,8 +11,8 @@ export function Welcome() {
         </Text>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        The Meta-DAO is the first organization in human history that&apos;s
-        governed by markets, not politics.
+        The Meta-DAO is the first organization in human history that&apos;s governed by markets, not
+        politics.
       </Text>
     </>
   );
