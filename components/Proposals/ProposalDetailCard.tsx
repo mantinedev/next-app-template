42b09a8db87f34f798decf73d07f95d99a98671d
Dodecahedr0x <hexadecifish@gmail.com>
2023-11-25 01:37:14 +0100

    Hide excess info

--- components/Proposals/ProposalDetailCard.tsx
+++ components/Proposals/ProposalDetailCard.tsx
@@ -1,5 +1,17 @@
 import { useCallback, useState } from 'react';
-import { Button, Divider, Fieldset, Flex, Group, Loader, Space, Stack, Text, TextInput } from '@mantine/core';
+import {
+  Accordion,
+  Button,
+  Divider,
+  Fieldset,
+  Flex,
+  Group,
+  Loader,
+  Space,
+  Stack,
+  Text,
+  TextInput,
+} from '@mantine/core';
 import Link from 'next/link';
 import { IconExternalLink } from '@tabler/icons-react';
 import { useProposal } from '@/hooks/useProposal';
@@ -10,6 +22,7 @@ import { ProposalOrdersCard } from './ProposalOrdersCard';
 import { ConditionalMarketCard } from '../Markets/ConditionalMarketCard';
 import { useExplorerConfiguration } from '@/hooks/useExplorerConfiguration';
 import { shortKey } from '@/lib/utils';
+import { StateBadge } from './StateBadge';
 
 export function ProposalDetailCard({ proposalNumber }: { proposalNumber: number }) {
   const { proposal, markets, orders, mintTokens, placeOrder, loading } = useProposal({
@@ -47,19 +60,6 @@ export function ProposalDetailCard({ proposalNumber }: { proposalNumber: number 
     [mintTokens, mintBaseAmount, mintQuoteAmount],
   );
 
-  const proposalState = () => {
-    if (!proposal) return null;
-    if (proposal.account.state.pending) {
-      return 'Pending';
-    }
-    if (proposal.account.state.passed) {
-      return 'Passed';
-    }
-    if (proposal.account.state.failed) {
-      return 'Failed';
-    }
-  };
-
   // const calculateTWAP = (twapOracle: TWAPOracle) => {
   //   const slotsPassed = twapOracle.lastUpdatedSlot.sub(twapOracle.initialSlot);
   //   const twapValue = twapOracle.observationAggregator.div(slotsPassed);
@@ -75,90 +75,108 @@ export function ProposalDetailCard({ proposalNumber }: { proposalNumber: number 
     </Group>
   ) : (
     <Stack gap="0">
-      <Flex
-        justify="flex-start"
-        align="flex-start"
-        direction="row"
-        wrap="wrap"
-      >
-        <Group>
-          <Stack>
-            <Text fw="bolder" size="xl">
-              Proposal #{proposal.account.number} {proposalState()}
-            </Text>
-            <Link href={proposal.account.descriptionUrl}>
-              <Group gap="sm">
-                <Text>Go to description</Text>
-                <IconExternalLink />
-              </Group>
-            </Link>
-            <Text>
-              Proposer{' '}
-              <a
-                href={generateExplorerLink(proposal.account.proposer.toString(), 'account')}
-                target="blank"
-              >
-                {shortKey(proposal.account.proposer.toString())}
-              </a>
-            </Text>
-            <Text>
-              Pass Market{' '}
-              <a
-                href={generateExplorerLink(proposal.account.openbookPassMarket.toString(), 'account')}
-                target="blank"
-              >
-                {shortKey(proposal.account.openbookPassMarket.toString())}
-              </a>
-            </Text>
-            <Text>
-              Fail Market{' '}
-              <a
-                href={generateExplorerLink(proposal.account.openbookFailMarket.toString(), 'account')}
-                target="blank"
-              >
-                {shortKey(proposal.account.openbookFailMarket.toString())}
-              </a>
-            </Text>
-            <Text>
-              Pass TWAP Market{' '}
-              <a
-                href={generateExplorerLink(proposal.account.openbookTwapPassMarket.toString(), 'account')}
-                target="blank"
-              >
-                {shortKey(proposal.account.openbookTwapPassMarket.toString())}
-              </a>
-            </Text>
-            <Text>
-              Fail TWAP Market{' '}
-              <a
-                href={generateExplorerLink(proposal.account.openbookTwapFailMarket.toString(), 'account')}
-                target="blank"
-              >
-                {shortKey(proposal.account.openbookTwapFailMarket.toString())}
-              </a>
-            </Text>
-            <Text>
-              Conditional USDC Vault{' '}
-              <a
-                href={generateExplorerLink(proposal.account.quoteVault.toString(), 'account')}
-                target="blank"
-              >
-                {shortKey(proposal.account.quoteVault.toString())}
-              </a>
-            </Text>
-            <Text>
-              Conditional META Vault{' '}
-              <a
-                href={generateExplorerLink(proposal.account.baseVault.toString(), 'account')}
-                target="blank"
-              >
-                {shortKey(proposal.account.baseVault.toString())}
-              </a>
-            </Text>
-          </Stack>
-        </Group>
+      <Flex justify="flex-start" align="flex-start" direction="row" wrap="wrap">
+        <Accordion w="100%" pb="md">
+          <Accordion.Item value={proposal.publicKey.toString()}>
+            <Accordion.Control>
+              <Stack>
+                <Group justify="space-between">
+                  <Text size="xl" fw={500}>
+                    Proposal #{proposal.account.number + 1}
+                  </Text>
+                  <StateBadge proposal={proposal} />
+                </Group>
+              </Stack>
+            </Accordion.Control>
+            <Accordion.Panel p="0" style={{ padding: '0' }}>
+              <Stack gap="sm">
+                <Link href={proposal.account.descriptionUrl}>
+                  <Group gap="sm">
+                    <Text>Go to description</Text>
+                    <IconExternalLink />
+                  </Group>
+                </Link>
+                <Text>
+                  Proposer{' '}
+                  <a
+                    href={generateExplorerLink(proposal.account.proposer.toString(), 'account')}
+                    target="blank"
+                  >
+                    {shortKey(proposal.account.proposer.toString())}
+                  </a>
+                </Text>
+                <Text>
+                  Pass Market{' '}
+                  <a
+                    href={generateExplorerLink(
+                      proposal.account.openbookPassMarket.toString(),
+                      'account',
+                    )}
+                    target="blank"
+                  >
+                    {shortKey(proposal.account.openbookPassMarket.toString())}
+                  </a>
+                </Text>
+                <Text>
+                  Fail Market{' '}
+                  <a
+                    href={generateExplorerLink(
+                      proposal.account.openbookFailMarket.toString(),
+                      'account',
+                    )}
+                    target="blank"
+                  >
+                    {shortKey(proposal.account.openbookFailMarket.toString())}
+                  </a>
+                </Text>
+                <Text>
+                  Pass TWAP Market{' '}
+                  <a
+                    href={generateExplorerLink(
+                      proposal.account.openbookTwapPassMarket.toString(),
+                      'account',
+                    )}
+                    target="blank"
+                  >
+                    {shortKey(proposal.account.openbookTwapPassMarket.toString())}
+                  </a>
+                </Text>
+                <Text>
+                  Fail TWAP Market{' '}
+                  <a
+                    href={generateExplorerLink(
+                      proposal.account.openbookTwapFailMarket.toString(),
+                      'account',
+                    )}
+                    target="blank"
+                  >
+                    {shortKey(proposal.account.openbookTwapFailMarket.toString())}
+                  </a>
+                </Text>
+                <Text>
+                  Conditional USDC Vault{' '}
+                  <a
+                    href={generateExplorerLink(proposal.account.quoteVault.toString(), 'account')}
+                    target="blank"
+                  >
+                    {shortKey(proposal.account.quoteVault.toString())}
+                  </a>
+                </Text>
+                <Text>
+                  Conditional META Vault{' '}
+                  <a
+                    href={generateExplorerLink(proposal.account.baseVault.toString(), 'account')}
+                    target="blank"
+                  >
+                    {shortKey(proposal.account.baseVault.toString())}
+                  </a>
+                </Text>
+              </Stack>
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
         <Space w="md" />
-        <Group>
+        <Group align="center" justify="center" m="auto">
           <Fieldset legend={`Mint conditional $${tokens?.meta?.symbol}`}>
             <TextInput
               label="Amount"
