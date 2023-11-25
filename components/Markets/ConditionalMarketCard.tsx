4a09f1f23cdb7da4197ce8016fe787e26e608674
Kollan House <kollanh@gmail.com>
2023-11-25 03:45:49 -0500

    feat: moves limit / market order to respective places, adds conditional checking for UX logic, single button

--- components/Markets/ConditionalMarketCard.tsx
+++ components/Markets/ConditionalMarketCard.tsx
@@ -6,11 +6,14 @@ import {
   Text,
   SegmentedControl,
   TextInput,
+  InputBase,
   Grid,
   GridCol,
   Flex,
   Button,
   Tooltip,
+  NativeSelect,
+  Pill,
 } from '@mantine/core';
 import { Icon12Hours } from '@tabler/icons-react';
 import { notifications } from '@mantine/notifications';
@@ -45,6 +48,7 @@ export function ConditionalMarketCard({
   const wallet = useWallet();
   const { fetchOpenOrders } = useAutocrat();
   const [orderType, setOrderType] = useState<string>('Limit');
+  const [orderSide, setOrderSide] = useState<string>('Buy');
   const [amount, setAmount] = useState<number>(0);
   const [price, setPrice] = useState<number>(0);
   const { crankMarketTransaction } = useOpenbookTwap();
@@ -102,44 +106,64 @@ export function ConditionalMarketCard({
         <Stack>
           <SegmentedControl
             style={{ marginTop: '10px' }}
+            data={['Buy', 'Sell']}
+            value={orderSide}
+            onChange={(e) => setOrderSide(e)}
+            fullWidth
+          />
+          <NativeSelect
+            style={{ marginTop: '10px' }}
             data={['Limit', 'Market']}
             value={orderType}
-            onChange={(e) => setOrderType(e)}
-            fullWidth
+            onChange={(e) => setOrderType(e.target.value)}
           />
           <TextInput
             label="Price"
             placeholder="Enter price..."
             type="number"
             onChange={(e) => setPrice(Number(e.target.value))}
+            disabled={orderType === 'Market'}
           />
-          <TextInput
+          <InputBase
             label="Amount of META"
             placeholder="Enter amount..."
             type="number"
+            rightSectionPointerEvents="none"
+            rightSectionWidth={100}
+            rightSection={
+              <>
+              {orderSide === 'Sell' ?
+                (
+                  Number(baseBalance) > 0 ? (
+                    <Pill>{baseBalance}</Pill>
+                  ) : null
+                ) : (
+                  quoteBalance && price ? (
+                    <Pill>
+                      {(Number(quoteBalance) / price).toFixed(0)}
+                    </Pill>
+                  ) : null
+                )
+              }
+              </>
+            }
             onChange={(e) => setAmount(Number(e.target.value))}
           />
           <Grid>
-            <GridCol span={6}>
+            <GridCol span={12}>
               <Button
                 fullWidth
-                color="green"
-                onClick={() =>
-                  placeOrder(amount, price, orderType === 'Limit', false, isPassMarket)
-                }
-                disabled={!amount || !price}
+                color={orderSide === 'Sell' ? 'red' : 'green'}
+                onClick={() => placeOrder(
+                  amount,
+                  price,
+                  orderType === 'Limit',
+                  orderSide === 'Sell',
+                  isPassMarket
+                  )}
+                disabled={!amount || (orderType !== 'Market' ? !price : false)}
               >
-                Bid
-              </Button>
-            </GridCol>
-            <GridCol span={6}>
-              <Button
-                fullWidth
-                color="red"
-                onClick={() => placeOrder(amount, price, orderType === 'Limit', true, isPassMarket)}
-                disabled={!amount || !price}
-              >
-                Ask
+                {orderSide} {isPassMarket ? 'p' : 'f'}META
               </Button>
             </GridCol>
           </Grid>
