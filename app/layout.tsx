80772b4044b57390621e9737ce6ba7af2490bdd7
Vitaly Rtishchev <rtivital@gmail.com>
2024-10-23 20:01:12 +0400

    Add example with Next.js 15

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: any }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <ColorSchemeScript />
         <link rel="shortcut icon" href="/favicon.svg" />
