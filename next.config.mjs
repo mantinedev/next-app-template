0bf58f521baa955f5d9e5de9c0dc58c43c32fbc6
Vitaly Rtishchev <rtivital@gmail.com>
2025-10-26 10:36:26 +0300

    Remove eslint from build

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,6 @@ const withBundleAnalyzer = bundleAnalyzer({
 
 export default withBundleAnalyzer({
   reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
